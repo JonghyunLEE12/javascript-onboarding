23c7352449911ef7146de4f65d2b2476d4d11652
JonghyunLEE12 <najo7609@gmail.com>
2023-10-01 00:55:53 +0900

    [ solve : Problem2 ]

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
   var answer;
-  return answer;
+  let prev = cryptogram
+
+  while (cryptogram.length > 0) {
+    for ( let i = 0 ; i < cryptogram.length-1; i ++) {
+      if (cryptogram[i] == cryptogram[i+1]) {
+        const remove = cryptogram[i] + cryptogram[i+1];
+        cryptogram = cryptogram.replace(remove,'');
+      }
+    }
+    if (prev == cryptogram) break;
+    prev = cryptogram;
+  }
+  return cryptogram
 }
 
 module.exports = problem2;
