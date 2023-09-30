4dda55472737eefffd611fd7c9c5a6a0f81d88e1
JonghyunLEE12 <najo7609@gmail.com>
2023-10-01 01:19:53 +0900

    [ solve : Problem3 ]

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,22 @@
 function problem3(number) {
   var answer;
-  return answer;
+  let result = 0;
+
+  const checking = (number) => {
+    number = String(number).split('');
+    if ( number.includes('3') || number.includes('6') || number.includes('9')) {
+      let count = 0;
+      if ( number.includes('3')) count = number.filter(value => value == 3).length;
+      if ( number.includes('6')) count = number.filter(value => value == 6).length;
+      if ( number.includes('9')) count = number.filter(value => value == 9).length;
+      return count;
+    }
+    return 0;
+  }
+  for ( let i = 0 ; i < number+1 ; i ++) {
+    result += checking(i);
+  }
+  return result;
 }
 
 module.exports = problem3;
