59a9fed4131cb4dac2663f54a5601b459b1f1ec8
JonghyunLEE12 <najo7609@gmail.com>
2023-10-01 13:18:58 +0900

    [ solve : Problem 4&5 ]

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
   var answer;
-  return answer;
+
+  const translate = (money) => {
+    const lst = [];
+    const currency = [50000,10000,5000,1000,500,100,50,10,1];
+    for (let cur of currency) { 
+      lst.push(parseInt(money / cur));
+      money = money % cur;
+    }
+   return lst
+  };
+  return translate(money);
 }
 
 module.exports = problem5;
